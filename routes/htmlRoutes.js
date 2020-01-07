10cb4b3d205d07ff37538095fc45a282d41318a4
Francis Rodson Clavel <frdclav@gmail.com>
2020-01-06 21:06:44 -0800

    fixes path part and adds in table.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,17 +1,19 @@
+var path = require("path");
+
 // HTML Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
 module.exports = function (app) {
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "home.html"));
+        res.sendFile(path.join(__dirname, "../home.html"));
     });
 
     app.get("/tables", function (req, res) {
-        res.sendFile(path.join(__dirname, "tables.html"));
+        res.sendFile(path.join(__dirname, "../tables.html"));
     });
 
     app.get("/reserve", function (req, res) {
-        res.sendFile(path.join(__dirname, "reserves.html"));
+        res.sendFile(path.join(__dirname, "../reserves.html"));
     });
 }
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT =  3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -32,6 +32,23 @@ var waitlist = [
     }
 ];
 require("./routes/htmlRoutes.js")(app);
+// // HTML Routes
+// // =============================================================
+
+// // Basic route that sends the user first to the AJAX Page
+
+//     app.get("/", function (req, res) {
+//         res.sendFile(path.join(__dirname, "home.html"));
+//     });
+
+//     app.get("/tables", function (req, res) {
+//         res.sendFile(path.join(__dirname, "tables.html"));
+//     });
+
+//     app.get("/reserve", function (req, res) {
+//         res.sendFile(path.join(__dirname, "reserves.html"));
+//     });
+
 require("./routes/apiRoutes.js")(app,table,waitlist);
 
 // Starts the server to begin listening
