b3ea8dab8a04efaee21a9cbf72c8f595eb27bf17
Francis Rodson Clavel <frdclav@gmail.com>
2020-01-06 19:15:00 -0800

    adds dummy data and adds app.listen line to server.js

--- server.js
+++ server.js
@@ -11,3 +11,29 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Tables (DATA)
+// =============================================================
+var table = [
+    {
+        "customerName": "rodson",
+        "phoneNumber": "1234567898",
+        "customerEmail": "f@g.com",
+        "customerID": "123"
+    }
+];
+
+var waitlist = [
+    {
+        "customerName": "waiting person",
+        "phoneNumber": "1234567898",
+        "customerEmail": "w@g.com",
+        "customerID": "waiting_forever"
+    }
+];
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
